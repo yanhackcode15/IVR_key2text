28d9f9897f1ec0d110ee7cf4be30eb93f31fbded
yanhackcode15 <iamyanhu@gmail.com>
2020-11-16 17:19:07 -0800

    port number change

--- fwd2txt.js
+++ fwd2txt.js
@@ -38,6 +38,6 @@ function sendSms(caller, twilioNumber) {
 
 
 // Create an HTTP server and listen for requests on port 3000
-console.log('Twilio Client app HTTP server running at http://127.0.0.1:80');
-app.listen(process.env.PORT);
+console.log('Twilio Client app HTTP server running at http://127.0.0.1');
+app.listen(process.env.PORT || 3000);
 
