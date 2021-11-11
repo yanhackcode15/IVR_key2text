e76c3b77e04c3c40e65eb22ca7372c5bf38da624
yanhackcode15 <iamyanhu@gmail.com>
2021-11-10 23:57:03 -0800

    added forward webhook quit

--- fwd2txt.js
+++ fwd2txt.js
@@ -5,6 +5,7 @@ const voiceResponse = require('twilio').twiml.VoiceResponse;
 
 const app = express();
 const port = process.env.PORT || 3000;
+const zenotiNumber = '16264697790';
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -19,12 +20,24 @@ app.all('/answer', (req, res) => {
   res.send(r.toString());
 });
 
+app.all('/forward', (req, res) => {
+  const caller = req.body.From;
+  const twilioNumber = req.body.To;
+  forwardToZenoti(caller, twilioNumber);
+
+  const r = new voiceResponse();
+  // r.say('Our online booking link has been sent. If you are calling from a landline, please hang up and call from a cell phone.');
+  r.play({},'https://sepia-cobra-7528.twil.io/assets/Gilmore%20Ave%2050.mp3');
+  res.send(r.toString());
+});
+
+
 function sendSms(caller, twilioNumber) {
   const accountSid = process.env.ACCOUNT_SID;
   const authToken = process.env.AUTH_TOKEN;
   const client = require('twilio')(accountSid, authToken);
   const message = {
-    body: "From Snipits-Pasadena: To book for tomorrow and later: www.calendly.com/snipitsmonrovia. For today: https://www.snipits.com/location/pasadena. ** Appts might be very limited this week. Please only book if you can make it. To be waitlisted or other questions? text us at (626)469-7790. **This number doesn't reply",
+    body: "From Snipits-Pasadena: To book for tomorrow and later: www.calendly.com/snipitsmonrovia. For today: https://www.snipits.com/location/pasadena. ** Appts might be very limited this week. Please only book if you can make it. To be waitlisted or other questions? text us at (626)469-7790.  **This number doesn't reply",
     from: twilioNumber,
     to: caller,
   };
@@ -36,6 +49,26 @@ function sendSms(caller, twilioNumber) {
       }
     })
     .done();
+}
+
+function forwardToZenoti(caller, twilioNumber){
+  const accountSid = process.env.ACCOUNT_SID;
+  const authToken = process.env.AUTH_TOKEN;
+  const client = require('twilio')(accountSid, authToken);
+  
+  const message = {
+    body: "Cancelation request from customer phonenumber "+caller,
+    from: twilioNumber,
+    to: zenotiNumber,
+  };
+  console.log({message})
+  return client.messages.create(message).then()
+    .catch(function(error) {
+      if (error.code === 21614) {
+        console.log("Uh oh, looks like this caller can't receive SMS messages.")
+      }
+    })
+    .done(); 
 }
 
 app.use((err, req, res, next) => {
